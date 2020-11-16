be602d96e21fceab7ef1965560b6530f2eea7898
ItzajanaOsuna <74266358+ItzajanaOsuna@users.noreply.github.com>
2020-11-15 18:21:53 -0600

    Registro y Login

--- app/src/main/java/com/gaby/micasaamigable/MainActivity.java
+++ app/src/main/java/com/gaby/micasaamigable/MainActivity.java
@@ -2,15 +2,82 @@ package com.gaby.micasaamigable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    int e;
-    int a;
+    EditText user_log, pass_log, user_reg, pass_reg;
+    Button btn_login, btn_reg, btn_reg2;
+    LinearLayout login1, login2, login3, reg1, reg2;
+
+    SharedPreferences pref ;//= getSharedPreferences("datos",MODE_PRIVATE); //documento
+    SharedPreferences.Editor editor; //=pref.edit();   //Editar documento
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //pref = getSharedPreferences("datos",MODE_PRIVATE); //documento
+        //editor=pref.edit();   //Editar documento
+
+
+        //editor.commit();
+
+        // String usr1=pref.getString("user1","-");
+
+        user_log=(EditText) findViewById(R.id.edt_user_log);
+        pass_log=(EditText) findViewById(R.id.edt_pass_log);
+        user_reg=(EditText) findViewById(R.id.edt_user_reg);
+        pass_reg=(EditText) findViewById(R.id.edt_pass_reg);
+        btn_login=(Button)findViewById(R.id.btn_start);
+        btn_reg=(Button)findViewById(R.id.btn_registro);
+        btn_reg2=(Button)findViewById(R.id.btn_registro2);
+        login1=findViewById(R.id.login1);
+        login2=findViewById(R.id.login2);
+        login3=findViewById(R.id.login3);
+        reg1=findViewById(R.id.registro1);
+        reg2=findViewById(R.id.registro2);
+
+    }
+
+    public void Login(View view){
+        new Restful(this, "login",
+                user_log.getText().toString(),
+                pass_log.getText().toString()).execute();
+    }
+
+    public void registro(View view){
+        reg1.setVisibility(View.VISIBLE);
+        reg2.setVisibility(View.VISIBLE);
+        login1.setVisibility(view.GONE);
+        login2.setVisibility(view.GONE);
+        login3.setVisibility(view.GONE);
+    }
+    public void NewRegistro(View view){
+        new Restful(this, "registro",
+                user_reg.getText().toString(),
+                pass_reg.getText().toString()).execute();
+    }
+    public void CancelReg(View view){
+        reg1.setVisibility(view.GONE);
+        reg2.setVisibility(view.GONE);
+        login1.setVisibility(view.VISIBLE);
+        login2.setVisibility(view.VISIBLE);
+        login3.setVisibility(view.VISIBLE);
+    }
+    public void registroOk(){
+        mensaje("Tu registro fue exitoso");
+    }
+
+    public void mensaje(String s){
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/gaby/micasaamigable/Restful.java
@@ -0,0 +1,177 @@
+package com.gaby.micasaamigable;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.UnknownHostException;
+
+/**
+ * Created by Carlos Moya on 20/02/2018.
+ */
+
+public class Restful  extends AsyncTask<Void,Void,String[][]> {
+
+    private String HTTP_RESTFUL;
+    private String url_p="https://pd2020.000webhostapp.com/";
+    private Context context;
+    private String info,nombre,pass;
+
+    public Restful(Context context, String info,String nombre,String pass) {
+        this.context = context;
+        this.info=info;
+        this.nombre=nombre;
+        this.pass=pass;
+        HTTP_RESTFUL=getURL(info);
+        Log.d("url","url:"+HTTP_RESTFUL);
+    }
+
+    /**
+     * Metodo que se conecta al RESTFUL para obtener un resultado
+     * */
+    public String[][] getRestFul()
+    {
+        String jsonResult="";
+        String[][] list3=new String[1][5];
+        String[][] listE=new String[1][1];
+        listE[0][0] = "0";
+        try {
+            URL url = new URL(HTTP_RESTFUL);
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.connect();
+
+            InputStream inputStream = connection.getInputStream();
+            jsonResult = inputStreamToString(inputStream).toString();//response.getEntity().getContent()).toString();
+            ////////////////////////////////////
+            int pos=jsonResult.indexOf('{');
+            int pos2=jsonResult.lastIndexOf('}');
+            if(pos==-1) { list3[0][0]="0"; return list3;}
+            String jsnres=jsonResult.substring(pos, pos2 + 1);
+            JSONObject object = new JSONObject(jsnres);
+            //obtiene el status
+            String status = object.getString("status");
+            System.out.print("status:"+status);
+            if( status.equals("50") )           //50 -> todo esta bien
+            {
+                //extrae los registros
+                JSONArray array = new JSONArray(object.getString("Registros"));
+                list3=new String[array.length()+1][5];
+                for (int i = 1; i <= array.length(); i++)
+                {
+                    //recorre cada registro y concatena el resultado
+                    JSONObject row = array.getJSONObject(i-1);
+                    switch(this.info)
+                    {
+                        case "registro":
+                            list3[0][0]=status;
+                            list3[0][1]=row.getString("respuesta");
+                            //list3[i][0]=row.getString("registro");
+                            break;
+                        case "login":
+                            list3[0][0]=status;
+                            list3[0][1]=row.getString("respuesta");
+                            break;
+                    }
+                }
+                return list3;
+            }
+            if( status.equals("500") )list3[0][0]="500";
+            else   list3[0][0]="0";
+            return list3;
+        }catch (UnknownHostException e)
+        {
+            listE[0][0] = e.getMessage();
+        }
+        catch (ClientProtocolException e) {
+            listE[0][0] = e.getMessage();
+            e.printStackTrace();
+        } catch (IOException e) {
+            listE[0][0] = e.getMessage();
+            e.printStackTrace();
+        } catch (JSONException e) {
+            listE[0][0] = e.getMessage();
+            e.printStackTrace();
+        }
+
+        return listE;
+    }
+
+    /**
+     * Transforma el InputStream en un String
+     * @return StringBuilder
+     * */
+    private StringBuilder inputStreamToString(InputStream is)
+    {
+        String line = "";
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader rd = new BufferedReader( new InputStreamReader(is) );
+        try
+        {
+            while( (line = rd.readLine()) != null )
+                stringBuilder.append(line);
+        }
+        catch( IOException e)
+        {
+            e.printStackTrace();
+        }
+        return stringBuilder;
+    }
+
+    @Override
+    protected  String[][] doInBackground(Void... arg0){
+        String[][] resul = getRestFul();
+        return resul;
+    }
+
+    @Override
+    protected void onPostExecute(String resul[][]) {
+        if(resul[0].length>0) {
+
+            Log.d("res", "res:" + resul[0][0].toString());
+            if (resul[0][0].equals("50")) {
+                switch (this.info) {
+                    case "registro":
+                        ((MainActivity)context).registroOk();
+                        break;
+                    case "login":
+
+
+                        //if(resul[0][1].toString().equals("ok"))
+                  //      ((MainActivity)context).datosOk("correcto");
+                        //else ((MainActivity)context).mensajes("verifica tus datos");
+                        break;
+                }
+            } else {
+                if (resul[0][0].equals("500")) {
+                    switch (this.info) {
+                        case "registro":
+                            //  ((Main) context).mensaje("Error en el registro, verifique sus datos");
+                            break;
+                    }
+                } //else ((Main) context).mensaje("Ocurrio un error \n Verifique su conexión");
+            }
+        }
+    }
+
+    public String getURL(String info)
+    {
+        switch(info)
+        {
+            case "registro": return url_p+"control.php?action=registro&user="+nombre+"&pass="+pass;
+            case "login": return url_p+"control.php?action=login&user="+nombre+"&pass="+pass;
+
+            default: return url_p+"control_p.php?action=error";
+        }
+    }
+}
